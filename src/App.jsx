5def4a3e5e4a7778457a7b16c3a7e64b0b82a526
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2023-02-08 14:23:09 +0200

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import Header from "./component/header/Header";
-// import Footer from "./component/footer/Footer";
 // import Introduction from "./component/main/introduction/Introduction";
 // import OurServices from "./component/main/our_services/OurServices";
 // import LatestWorks from "./component/main/latest_works/LatestWorks";
 // import OurTeam from "./component/main/our_team/OurTeam";
 // import Review from "./component/main/review/Review";
 // import Conaction from "./component/main/conaction/Conaction";
+import Footer from "./component/footer/Footer";
 
 function App() {
   return (
@@ -17,8 +17,8 @@ function App() {
       <LatestWorks />
       <OurTeam />
       <Review />
-      <Conaction />
-      <Footer /> */}
+      <Conaction />*/}
+      <Footer /> 
     </div>
   );
 }
--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import s from "./Footer.module.css"
+import facebook from "../../assets/images/facebook.png"
+import twitter from "../../assets/images/twitter.png"
+import pinterest from "../../assets/images/pinterest.png"
 
 const Footer = () => {
   return (
-    <section className={s.wrapper}>Footer</section>
+    <section className={s.wrapper}>
+      <div className={s.contacts_info}>
+        <p className={s.contacts_info_text}>3rd Floor, Santa Monica Beach, CA, San Francisco</p>
+        <div className={s.contacts_phone_and_number}>
+          <a href="number">(359) 0 123 456 789</a>
+          <p> - </p>
+          <a href="email">info@yourmail.com</a>
+        </div>
+        <div className={s.social_media}>
+          <a href="http://facebook.com">
+            <img src={facebook} alt="facebook" />
+          </a>
+          <a href="http://twitter.com">
+            <img src={twitter} alt="twitter" />
+          </a>
+          <a href="http://pinterest.com">
+            <img src={pinterest} alt="pinterest" />
+          </a>
+        </div>
+      </div>
+
+      <div className={s.reserved}>
+        <p>Copyright 2016 - All Rights Reserved - Designed by JD</p>
+      </div>
+    </section>
   )
 }
 
